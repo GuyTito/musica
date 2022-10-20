db4aa826d5ba7e77d4bd3d07b52d9cb409f01d80
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-10-20 16:45:47 +0000

    added style guide

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-alt": "rgba(26, 30, 31, 1)",
+        "secondary": "#FACD66",
+        "light": "rgba(239, 238, 224, 1)",
+        "light-o": "rgba(239, 238, 224, 0.25)",
+        "primary": "#A4C7C6",
+        "dark": "#1D2123",
+        "alt": "#A4C7C6",
+      },
+      fontFamily: {
+        serif: ["Quicksand", ...defaultTheme.fontFamily.serif],
+      },
+    },
   },
   plugins: [],
 }
