3ea653aab0104821bc8005af4de65310f5818303
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-10-21 09:36:03 +0000

    added top charts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,6 +8,8 @@ import logo from "../assets/logo.png";
 import search from "../assets/icons/search.png";
 import hug from "../assets/hug.png";
 import heart from "../assets/icons/heart.svg";
+import golden from "../assets/golden-age.png";
+import HoleheartSVG from "../assets/icons/HoleheartSVG";
 
 
 export default function Home() {
@@ -38,9 +40,9 @@ export default function Home() {
             <input type="text" className="text-light bg-transparent placeholder-white/25 text-sm w-full py-6 outline-none" placeholder="Search tracks, artists..." />
           </form>
 
-          <div>
+          <div className="flex gap-5 mr-[60px] ">
             {/* hero  section */}
-            <div className="bg-[#609EAF] w-[686px] py-7  px-10 rounded-[40px] ">
+            <div className="bg-[#609EAF] w-[60%] py-7  px-10 rounded-[40px] ">
               <span className="text-xs">Currated playlist</span>
               <div className="my-20 space-y-2">
                 <h1 className="font-bold text-4xl">R & B Hits</h1>
@@ -55,7 +57,28 @@ export default function Home() {
                 <span className="text-sm">33k Likes</span>
               </div>
             </div>
-            <div></div>
+
+            {/* top charts */}
+            <div className=" w-[40%]">
+              <h2 className="mb-[14px] text-2xl font-bold">Top Charts</h2>
+              <div className="space-y-3 ">
+                <div className="flex items-center justify-between p-4 rounded-[20px] bg-dark-alt ">
+                  <div className="flex items-center gap-4 ">
+                    <img src={golden} className="rounded-[10px]" alt="" />
+                    <div className="flex flex-col gap-1">
+                      <h3 className="text-base">Golden age of 80s</h3>
+                      <span className="text-white/50 text-xs">Sean Swadder</span>
+                      <span className="text-xs">2:34:45</span>
+                    </div>
+                  </div>
+                  <div>
+                    <button className="p-[10px] rounded-full border border-light-o">
+                      <HoleheartSVG />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
