9a1004efc8c3b82c413bb5b87f9c9c20d2b45c93
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-10-21 12:36:30 +0000

    music player

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,6 +11,10 @@ import heart from "../assets/icons/heart.svg";
 import golden from "../assets/golden-age.png";
 import bubble from "../assets/bubble.png";
 import HoleheartSVG from "../assets/icons/HoleheartSVG";
+import { TfiControlShuffle } from "react-icons/tfi";
+import { FaPlay, FaVolumeOff, FaVolumeUp } from "react-icons/fa";
+import { TbRepeatOnce, TbRepeat } from "react-icons/tb";
+import { BsSkipEndFill, BsSkipStartFill } from "react-icons/bs";
 
 
 export default function Home() {
@@ -34,14 +38,14 @@ export default function Home() {
             </div>
         </nav>
 
-        <div className="ml-6 w-full">
-          {/* topnav */}
-          <form className=" pl-7 flex items-center">
+        <div className="ml-6 w-full mb-5 mr-[60px]">
+          {/* search bar */}
+          <form className=" pl-7 flex items-center w-[60%]">
             <img src={search} alt='search' className="mr-5" />
             <input type="text" className="text-light bg-transparent placeholder-white/25 text-sm w-full py-6 outline-none" placeholder="Search tracks, artists..." />
           </form>
 
-          <div className="flex gap-5 mr-[60px] ">
+          <div className="flex gap-5">
             {/* hero  section */}
             <div className="bg-[#609EAF] w-[60%] py-7  px-10 rounded-[40px] ">
               <span className="text-xs">Currated playlist</span>
@@ -89,6 +93,48 @@ export default function Home() {
               <div>
                 <img src={bubble} className="rounded-3xl" alt="" />
                 <span className="text-xs">Life in a bubble</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* music player */}
+        <div className="fixed backdrop-blur bg-dark-alt/50 border-t border-white/10 w-full  bottom-0 flex justify-between items-center pl-[100px] pr-[60px] py-6">
+          {/* cover image */}
+          <div className="flex items-center gap-3">
+            <img src={golden} className="w-12 h-12 rounded-[14px]" alt="" />
+            <div className="flex flex-col">
+              <h3 className="font-bold text-sm">Seasons in</h3>
+              <span className="font-bold text-[10px] text-xs  text-white/40">James</span>
+            </div>
+          </div>
+
+          {/* music controls */}
+          <div className="flex flex-col gap-8 items-center">
+            <div className="flex gap-10">
+              <TfiControlShuffle className="w-6 h-6" />
+              <BsSkipStartFill className="w-6 h-6" />
+              <button className="rounded-full bg-secondary p-2 shadow-[0px_0px_18px_rgba(255,255,255,0.3)]">
+                <FaPlay className="w-3 h-3" />
+              </button>
+              <BsSkipEndFill className="w-6 h-6" />
+              <TbRepeatOnce className="w-6 h-6" />
+            </div>
+            <div className="w-[600px] h-[2px] rounded bg-white/10 flex items-center">
+              <div className="bg-secondary w-1/2 h-[2px] rounded-l"></div>
+              <div className="p-[2px] bg-transparent rounded-full border-white border">
+                <div className="bg-secondary h-2 w-2 rounded-full "></div>
+              </div>
+            </div>
+          </div>
+
+          {/* volume */}
+          <div className="flex gap-2 items-center">
+            <FaVolumeUp />
+            <div className="w-[150px] h-[2px] rounded bg-white/10 flex items-center">
+              <div className="bg-secondary w-1/2 h-[2px] rounded-l"></div>
+              <div className="p-[2px] bg-transparent rounded-full border-white border">
+                <div className="bg-secondary h-2 w-2 rounded-full "></div>
               </div>
             </div>
           </div>
