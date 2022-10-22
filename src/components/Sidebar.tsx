53e8eeb4773584c94f2d6f34e25b55c2756c9df0
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-10-22 12:13:35 +0000

    Create Sidebar.tsx

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,30 @@
+import HomeSVG from "../assets/icons/HomeSVG";
+import LogoutSVG from "../assets/icons/LogoutSVG";
+import PlaylistSVG from "../assets/icons/PlaylistSVG";
+import ProfileSVG from "../assets/icons/ProfileSVG";
+import RadioSVG from "../assets/icons/RadioSVG";
+import VideosSVG from "../assets/icons/VideosSVG";
+import logo from "../assets/logo.png";
+
+
+export default function Sidebar() {
+  
+  
+  return (
+    <>
+      <nav className="mt-6 ml-6">
+        <div><img src={logo} alt="logo" /></div>
+        <div className="mt-10 bg-dark-alt py-6 px-4 rounded-full space-y-7">
+          <HomeSVG />
+          <PlaylistSVG />
+          <VideosSVG />
+          <RadioSVG />
+        </div>
+        <div className="mt-10 bg-dark-alt py-6 px-4 rounded-full space-y-7">
+          <ProfileSVG />
+          <LogoutSVG />
+        </div>
+      </nav>
+    </>
+  )
+}
