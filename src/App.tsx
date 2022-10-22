bf13e0e4acf046bf00529801fb1fc49a23ec41b5
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-10-22 12:44:20 +0000

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
 import Home from "./pages/Home";
+
 
 export default function App() {
   
   
   return (
     <>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
