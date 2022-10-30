61aab546c4b3bf9f99deb073995edae6efd665d3
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-10-30 13:31:23 +0000

    TracksData type

--- src/types.ts
+++ src/types.ts
@@ -4,10 +4,24 @@ export interface IconProps {
 
 export type RepeatOptions = 'ONE' | 'ENABLED' | 'DISABLED'
 
-export interface SongType {
+export interface SongData {
   artist: string
   title: string
   audio: string
   duration: string
   cover: string
 }
+
+export type TracksData = Array<{
+  album: {
+    cover: Array<{
+      url: string
+    }>
+  }
+  artists: Array<{
+    name: string
+  }>
+  id: string
+  name: string
+  durationText: string
+}>
