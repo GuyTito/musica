3bad9525083c1efb71abfad1490cb50f766b0d28
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-10-30 09:24:16 +0000

    modal for search results

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,15 +1,39 @@
+import { FormEvent, useState } from "react";
 import search from "../assets/icons/search.png";
 
 
 export default function Searchbar() {
+  const [modal, setModal] = useState(false)
+  const [searchTerm, setSearchTerm] = useState("")
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    setModal(true)
+  }
+
+  function closeModal(){
+    setSearchTerm("")
+    setModal(false)
+  }
   
   
   return (
     <>
-      <form className=" pl-7 flex items-center w-[60%]">
+      {/* search bar */}
+      <form onSubmit={(e)=>handleSubmit(e)} className=" pl-7 flex items-center w-[60%]">
         <img src={search} alt='search' className="mr-5" />
-        <input type="text" className="text-light bg-transparent placeholder-white/25 text-sm w-full py-6 outline-none" placeholder="Search tracks, artists..." />
+        <input type="text" value={searchTerm} 
+          onChange={(e) => setSearchTerm(e.target.value)} 
+          className="text-light bg-transparent placeholder-white/25 text-sm w-full py-6 outline-none" 
+          placeholder="Search songs" />
       </form>
+
+      {/* search result modal */}
+      {modal && 
+        <div className="absolute bg-dark inset-0 top-[68px]">
+          <button onClick={()=>closeModal()} className="py-1 px-[10px] rounded-full border-[3px] border-secondary text-secondary absolute right-20">&#x2715;</button>
+        </div>
+      }
     </>
   )
 }
