b96a1b4356b248d362194dfc1cc712be12c73b91
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-11-08 00:10:03 +0000

    complete html structure

--- src/pages/Playlist.tsx
+++ src/pages/Playlist.tsx
@@ -1,14 +1,48 @@
+import { FaHeart } from "react-icons/fa"
 import { useParams } from "react-router-dom"
+import HoleheartSVG from "../assets/icons/HoleheartSVG"
 import { useSongsContext } from "../context/SongsContext"
+import { totalDuration } from "../hooks/useHooks"
+import { PlaylistType } from "../types"
 
 export default function Playlist() {
   const { id } = useParams()
   const { playlists } = useSongsContext()
-  const playlist = playlists.find(playlist => playlist.id === id)
+  const playlist = playlists.find(playlist => playlist.id === id) as PlaylistType 
   
   return (
     <>
-      {playlist ? playlist.title : `Playlist with id of "${id}" not available`}
+      {/* {playlist ? playlist.title : `Playlist with id of "${id}" not available`} */}
+      <div>
+        {/* header */}
+        <div>
+          <img src={playlist?.cover} className="" alt="" />
+          <div>
+            <h1>{playlist?.title}</h1>
+            <p>{playlist?.info}</p>
+            <div>
+              <span>{playlist?.files.length} songs</span> - <span>{ totalDuration(playlist.files)}</span>
+            </div>
+            <div>
+              <button>Play all</button>
+              <button>Add to collection</button>
+              <button className="text-red-500"><FaHeart /></button>
+            </div>
+          </div>
+        </div>
+
+        {/* songs */}
+        <div>
+          {playlist.files.map(song => (
+            <div key={song.id}>
+              <img src={song.cover} alt="" />
+              <button><HoleheartSVG /></button>
+              <span>{song.title}</span>
+              <span>{song.duration}</span>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   )
 }
