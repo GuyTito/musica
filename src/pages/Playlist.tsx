21281ffef090413ae99b1e0ab3d70fb64ac1502e
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-11-09 11:12:56 +0000

    play or pause playlist

--- src/pages/Playlist.tsx
+++ src/pages/Playlist.tsx
@@ -1,4 +1,4 @@
-import { FaHeart, FaPlay } from "react-icons/fa"
+import { FaHeart, FaPause, FaPlay } from "react-icons/fa"
 import { useParams } from "react-router-dom"
 import HoleheartSVG from "../assets/icons/HoleheartSVG"
 import { useSongsContext } from "../context/SongsContext"
@@ -11,12 +11,13 @@ export default function Playlist() {
   const { id } = useParams()
   const { playlists } = useSongsContext()
   const playlist = playlists.find(playlist => playlist.id === id) as PlaylistType 
+  const { playSongs, isPlaying, queue, changePlayState } = useSongsContext()
 
   useEffect(() => {
     // 👇️ set style on body element
     document.body.style.background = 'center/cover fixed no-repeat';
     document.body.style.backgroundPosition = '50% 20%';
-    document.body.style.backgroundImage = `linear-gradient(rgba(29, 33, 35, 0.9), rgba(29, 33, 35, 0.99)), url(${playlist.cover})`;
+    document.body.style.backgroundImage = `linear-gradient(rgba(29, 33, 35, 0.9), rgba(29, 33, 35, 0.99)), url(${playlist && playlist?.cover})`;
 
     return () => {
       // 👇️ optionally remove styles when component unmounts
@@ -25,6 +26,36 @@ export default function Playlist() {
       document.body.classList.add('bg-dark');
     };
   }, []);
+
+  function handlePlaySongs(){
+    if (queue === (playlist && playlist?.files)){
+      if (isPlaying){
+        changePlayState(false)
+      }else{
+        changePlayState(true)
+      }
+    }else {
+      playSongs(playlist.files)
+    }
+  }
+
+  const playButton = (
+    <>
+      <span className="flex justify-center items-center w-3 h-3 rounded-full bg-secondary">
+        <FaPlay className="w-3 h-3 m-1 text-black" />
+      </span>
+      <span>Play</span>
+    </>
+  )
+
+  const pauseButton = (
+    <>
+      <span className="flex justify-center items-center w-3 h-3 rounded-full bg-secondary">
+        <FaPause className="w-3 h-3 m-1 text-black" />
+      </span>
+      <span>Pause</span>
+    </>
+  )
   
   return (
     <>
@@ -42,11 +73,10 @@ export default function Playlist() {
                 </div>
               </div>
               <div className="flex gap-5 items-center self-end mb-2">
-                <button className="flex items-center gap-2 py-1 px-3 rounded-full bg-white/10 active:bg-white/5 hover:bg-white/20">
-                  <button className="flex justify-center items-center w-3 h-3 rounded-full bg-secondary">
-                    <FaPlay className="w-3 h-3 m-1 text-black" />
-                  </button>
-                  <span>Play all</span>
+                <button onClick={() => handlePlaySongs()} className="flex items-center gap-2 py-1 px-3 rounded-full bg-white/10 active:bg-white/5 hover:bg-white/20">
+                  {(queue !== (playlist && playlist?.files)) ? playButton
+                    : !isPlaying ? playButton : pauseButton
+                  }
                 </button>
                 <button className="flex items-center gap-2 py-1 px-3 rounded-full bg-white/10 active:bg-white/5 hover:bg-white/20">
                   <img src={addtocol} alt="" />
@@ -59,8 +89,8 @@ export default function Playlist() {
 
           {/* songs */}
           <div className="mt-12 space-y-3">
-            {playlist.files.map(song => (
-              <div key={song.id} className="flex justify-between items-center bg-white/10 rounded-lg p-2 hover:bg-white/20" title="Play">
+            {playlist.files.map((song, index) => (
+              <div onClick={() => playSongs(playlist.files, index)} key={song.id} className="flex justify-between items-center bg-white/10 rounded-lg p-2 hover:bg-white/20" title="Play">
                 <div className="flex gap-5 items-center">
                   <img src={song.cover} className="w-10 h-10 rounded-lg" alt="" />
                   <button><HoleheartSVG /></button>
@@ -71,7 +101,8 @@ export default function Playlist() {
             ))}
           </div>
         </div>
-        : `Playlist with id of "${id}" not available`}
+        : `Playlist with id of "${id}" not available`
+      }
     </>
   )
 }
