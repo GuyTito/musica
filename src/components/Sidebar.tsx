e631b6075b06e97fcfe477c823bc1717250c5299
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-11-21 16:27:59 +0000

    dynamic background with url

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,5 @@
-import { NavLink } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { NavLink, useLocation } from "react-router-dom";
 import HomeSVG from "../assets/icons/HomeSVG";
 import LogoutSVG from "../assets/icons/LogoutSVG";
 import PlaylistSVG from "../assets/icons/PlaylistSVG";
@@ -10,11 +11,17 @@ import { SidebarProps } from "../types";
 
 
 export default function Sidebar({ setMenu }: SidebarProps) {
-  
+  const [isNavBg, setIsNavBg] = useState(false)
+  const location = useLocation()
+
+  useEffect(()=>{
+    if (location.pathname.includes('playlist')) setIsNavBg(true)
+  })
+
   
   return (
     <>
-      <nav className="sm:mt-6 sm:mx-6 bg-dark">
+      <nav className={`sm:mt-6 sm:mx-6 ${isNavBg || 'bg-dark'}`}>
         <div className="hidden sm:block"><img src={logo} alt="logo" /></div>
         <div className="sm:mt-10 sm:bg-dark-alt py-6 px-6 sm:px-4 rounded-full flex flex-col gap-7">
           <NavLink onClick={() => setMenu?.(false)} to="home" title="Home">
@@ -41,7 +48,7 @@ export default function Sidebar({ setMenu }: SidebarProps) {
             <RadioSVG /> <span className="sm:hidden">Radios</span>
           </div>
         </div>
-        <div className="sm:mt-10 sm:bg-dark-alt pb-6 sm:pb-0 sm:py-6 px-6 sm:px-4 rounded-full space-y-7">
+        <div className="sm:mt-10 sm:bg-dark-alt pb-6 sm:py-6 px-6 sm:px-4 rounded-full space-y-7">
           <div onClick={() => setMenu?.(false)} className="flex gap-3 items-center">
             <ProfileSVG /> <span className="sm:hidden">Profile</span>
           </div>
