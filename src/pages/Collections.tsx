cca6dbeb46cdf14d1a71bf8dbe607b75f41e096f
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-11-21 16:28:26 +0000

    style: hover effect for card

--- src/pages/Collections.tsx
+++ src/pages/Collections.tsx
@@ -26,11 +26,12 @@ export default function Collections() {
           {/* my collections */}
           <div className="flex items-center gap-6">
             {myPlaylists.length > 0 && myPlaylists.map((playlist: PlaylistType) => ( 
-              <div onClick={() => navigate(`/playlist/${playlist.id}`)} key={playlist?.id} className="relative rounded-[20px] w-[213px] h-[234px] overflow-hidden cursor-pointer">
+              <div onClick={() => navigate(`/playlist/${playlist.id}`)} key={playlist?.id} className="relative rounded-[20px] w-[213px] h-[234px] overflow-hidden cursor-pointer group">
                 <img src={playlist?.cover} className="absolute object-cover w-full h-full object-center" alt="" />
                 <div className="absolute z-30 bottom-[22px] ml-5 flex flex-col">
-                  <span className="text-2xl text-dark font-semibold">{playlist?.title}</span>
+                  <span className="text-2xl text-white/70 group-hover:text-white font-semibold">{playlist?.title}</span>
                 </div>
+                <div className="absolute h-full w-full z-20 bg-black/50 group-hover:bg-black/70"></div>
               </div>)
             )}
           </div>
